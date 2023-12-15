21d68d22e9263eee5e784de8b69157cf8c1e6764
antobevi <abevilacqua@frba.utn.edu.ar>
2023-12-14 21:41:16 -0300

    Agrego ejemplo sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+-- Esto es util para hacer demostraciones o probar
+
+-- Insertamos datos de ejemplo en la tabla Owner
+INSERT INTO Owner(name, surname, address)
+VALUES('Maximiliano', 'Sandoval', 'Calle 123');
+INSERT INTO Owner(name, surname, address)
+VALUES('Natalia', 'Cirullo', 'Calle 456');
+INSERT INTO Owner(name, surname, address)
+VALUES('Antonella', 'Bevilacqua', 'Calle 789');
+INSERT INTO Owner(name, surname, address)
+VALUES('Sergio', 'Benicio', 'Calle 101');
+INSERT INTO Owner(name, surname, address)
+VALUES('Brian', 'Mermelstein', 'Calle 112');
+
+-- Insertamos datos de ejemplo en la tabla Pet
+INSERT INTO Pet(name, age, species, owner)
+VALUES('Chano', 2, 'Apiradora Robot', 1);
+INSERT INTO Pet(name, age, species, owner)
+VALUES('Pepe', 1, 'Sapo', 2);
+INSERT INTO Pet(name, age, species, owner)
+VALUES('Morita', 10, 'Perro', 3);
+INSERT INTO Pet(name, age, species, owner)
+VALUES('Bondiola', 3, 'Perro', 4);
+INSERT INTO Pet(name, age, species, owner)
+VALUES('Momo', 2, 'Gato', 5);
